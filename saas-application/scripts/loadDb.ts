94de9beaa60e32da35cda065cff6f6a6d5b4b9be
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2024-11-09 18:06:24 +0100

    dd
    add

--- saas-application/scripts/loadDb.ts
+++ saas-application/scripts/loadDb.ts
@@ -3,4 +3,19 @@ import { DataAPIClient, VectorDoc, UUID, ObjectId } from '@datastax/astra-db-ts'
 
 import { PuppeteerWebBaseLoader } from "@langchain/community/document_loaders/web/puppeteer";
 
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
+
+import "dotenv/config"
+import OpenAI from 'openai';
+
+const {OPENAI_API_KEY, ASTRA_DB_APPLICATION_TOKEN, ASTRA_DB_API_ENDPOINT , ASTRA_DB_COLLECTION,ASTRA_DB_NAMESPACE} = process.env
+
+const openai = new OpenAI({apiKey: OPENAI_API_KEY})
+
+
+
+
+const data = [
+    
+]
