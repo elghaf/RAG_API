36a1af4d29cecc910eb948defccf11cb508305f1
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2024-11-09 16:42:25 +0100

    add

---
+++ saas-app/src/scripts/loadDb.tsx
@@ -0,0 +1,7 @@
+import { DataAPIClient, VectorDoc, UUID, ObjectId } from '@datastax/astra-db-ts';
+
+import { PuppeteerWebBaseLoader } from "@langchain/community/document_loaders/web/puppeteer";
+
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import OpenAI from "openai"
+import "dotenv/config"
