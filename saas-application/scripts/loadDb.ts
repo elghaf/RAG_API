c48a65c6b14246d95abb08a7b7d8ffa5f5355df9
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2024-11-09 17:21:47 +0100

    add

---
+++ saas-application/scripts/loadDb.ts
@@ -0,0 +1,6 @@
+
+import { DataAPIClient, VectorDoc, UUID, ObjectId } from '@datastax/astra-db-ts';
+
+import { PuppeteerWebBaseLoader } from "@langchain/community/document_loaders/web/puppeteer";
+
+
